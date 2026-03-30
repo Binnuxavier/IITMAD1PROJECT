fa4fd5bc59b2fc61f056238f593b1d0478caf13e
Binnuxavier <23F3003846@ds.study.iitm.ac.in>
2026-03-30 21:26:21 +0530

    Webpage Framework

--- app.py
+++ app.py
@@ -4,7 +4,46 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello IIT Project"
+    return "Welcome to placement portal"
 
+@app.route("/login")
+def login():
+    return "Login Page"
+
+@app.route("/student/register")
+def student_register():
+    return "Student Register"
+
+@app.route("/company/register")
+def company_register():
+    return "Company Registration"
+
+@app.route("/student/dashboard")
+def student_dashboard():
+    return "Student Dashboard"
+
+@app.route("/company/dashboard")
+def company_dashboard():
+    return "Company Dashboard"
+
+@app.route("/admin/dashboard")
+def admin_dashboard():
+    return "Admin Dashboard"
+
+@app.route("/job/post")
+def job_post():
+    return "Job Post"
+
+@app.route("/job/apply")
+def job_apply():
+    return "Job Apply"
+
+@app.route("/apply/status")
+def apply_status():
+    return "Apply Status"
+
+@app.route("/about")
+def about():
+    return "About"
 if __name__=="__main__":
     app.run(debug=True)
