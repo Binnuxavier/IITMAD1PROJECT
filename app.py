100fc1b8ba0fb0e9393659ac7ad5f4431b823470
Binnuxavier <23F3003846@ds.study.iitm.ac.in>
2026-04-02 02:44:50 +0530

    .

--- app.py
+++ app.py
@@ -1,49 +1,74 @@
-from flask import Flask
+from flask import Flask, render_template,request,redirect,url_for
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to placement portal"
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
-    return "Login Page"
+    return render_template("login.html")
 
-@app.route("/student/register")
+@app.route("/student/register", methods =["GET","POST"])
 def student_register():
-    return "Student Register"
+    if request.method=="POST":
+        student_id= request.form.get("student_id")
+        student_name=request.form.get("student_name")
+        program_name=request.form.get("program_name")
+        mobile_number=request.form.get("mobile_number")
+        email_id=request.form.get("email_id")       
 
-@app.route("/company/register")
+        print(student_id)
+        print(student_name)
+        print(program_name)
+        print(mobile_number)
+        print(email_id)
+        return ("Data entered successfully")
+    
+    return render_template("studentregister.html")
+
+@app.route("/company/register",methods=["GET","POST"])
 def company_register():
-    return "Company Registration"
+    if request.method=="POST":
+        company_id=request.form.get("company_id")
+        company_name=request.form.get("company_name")
+        mobile_number=request.form.get("mobile_number")
+        email_id=request.form.get("email_id")
+        print(company_id)
+        print(company_name)
+        print(mobile_number)
+        print(email_id)
+        return("Registered successfully")
+
+    return render_template("companyregister.html")
 
 @app.route("/student/dashboard")
 def student_dashboard():
-    return "Student Dashboard"
+    return render_template("studentdashboard.html")
 
 @app.route("/company/dashboard")
 def company_dashboard():
-    return "Company Dashboard"
+    return render_template("companydashboard.html")
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
-    return "Admin Dashboard"
+    return render_template("admindashboard.html")
 
 @app.route("/job/post")
 def job_post():
-    return "Job Post"
+    return render_template("jobposting.html")
 
 @app.route("/job/apply")
 def job_apply():
-    return "Job Apply"
+    return render_template("jobapply.html")
 
 @app.route("/apply/status")
 def apply_status():
-    return "Apply Status"
+    return render_template("jobstatus.html")
 
 @app.route("/about")
 def about():
-    return "About"
+    return render_template("about.html")
 if __name__=="__main__":
     app.run(debug=True)
